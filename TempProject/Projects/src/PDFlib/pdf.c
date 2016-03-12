768095b781749826f208fe75e17a221c20e16687
HuanDeng <huan_d@sina.com>
2016-03-12 19:44:38 +0800

    update

--- TempProject/Projects/src/PDFlib/pdf.c
+++ TempProject/Projects/src/PDFlib/pdf.c
@@ -9,16 +9,16 @@ char DataLineBuf[DATA_POINT_LENGTH];
 void PDF_Gen_Func(void)
 {
 	int lop=0;
-	PdfGobRes = f_mount(0,&PdfFileSystem);						//挂载文件系统
-	PdfGobRes=f_open(&DataLineFile,"0:demo.txt",FA_READ);		//初始化读文件
-	PdfGobRes=f_open(&PDFFile,"0:2ptmp.pdf",FA_WRITE);			//初始化写文件
-	PDFFile.fptr=DATA_START_ADDR;								//移动文件指针到数据条地址
-	for(lop=0;lop<10;lop++)										//测试插入10条数据
+	PdfGobRes = f_mount(0,&PdfFileSystem);												//挂载文件系统
+	PdfGobRes=f_open(&DataLineFile,"0:demo.txt",FA_READ);					//初始化读文件
+	PdfGobRes=f_open(&PDFFile,"0:2ptmp.pdf",FA_WRITE);						//初始化写文件
+	PDFFile.fptr=DATA_START_ADDR;																	//移动文件指针到数据条地址
+	for(lop=0;lop<10;lop++)																				//测试插入10条数据
 	{
 		PdfGobRes=f_read(&DataLineFile,DataLineBuf,DATA_POINT_LENGTH,&PdfByte2Read);
-		DataLineFile.fptr+=2;									//过滤\r\n
+		DataLineFile.fptr+=2;																				//过滤\r\n
 		PdfGobRes=f_write(&PDFFile,DataLineBuf,DATA_POINT_LENGTH,&PdfByte2Write);
-		PDFFile.fptr+=DATA_POINT_OFFSET;						//移动指针到下一条数据
+		PDFFile.fptr+=DATA_POINT_OFFSET;														//移动指针到下一条数据
 	}
 	f_close(&DataLineFile);
 	f_close(&PDFFile);
